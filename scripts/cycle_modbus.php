9afbe5b25489d198110e5e29db227bed73592599
Sergei Jeihala <jey@tut.by>
2015-09-22 12:25:21 +0300

    Minor tweak

--- scripts/cycle_modbus.php
+++ scripts/cycle_modbus.php
@@ -40,7 +40,7 @@ while (1)
    // check all web vars
    $modbus->readAll();
 
-   if (file_exists('./reboot') || $_GET['onetime'])
+   if (file_exists('./reboot') || IsSet($_GET['onetime']))
    {
       $db->Disconnect();
       exit;
